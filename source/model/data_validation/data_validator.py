d7c5d42ac7b7144e94d455967dd42ba3b8953e22
steven-snelling <stevensnelling@hotmail.com>
2017-10-04 21:31:21 +1300

    Bad Smell 2 - added appending data to cleaned_person array inside newly created for loop and commented out existing if statements and tested that behaviour is still correct.

--- source/model/data_validation/data_validator.py
+++ source/model/data_validation/data_validator.py
@@ -26,34 +26,35 @@ class DataValidator(IDataValidator):
 
                         if self.__test_data(str(dirty_person[index]), patterns[index]):
                             print("Valid data: " + str(dirty_person[index]))
+                            cleaned_person.append(str(dirty_person[index]))
 
-                    if self.__test_data(str(dirty_person[0]), patterns[0]):
-                        print("Valid empid: " + str(dirty_person[0]))
-                        cleaned_person.append(str(dirty_person[0]))
-
-                    if self.__test_data(str(dirty_person[1]), patterns[1]):
-                        print("Valid gender: " + str(dirty_person[1]))
-                        cleaned_person.append(str(dirty_person[1]))
-
-                    if self.__test_data(str(dirty_person[2]), patterns[2]):
-                        print("Valid age: " + str(dirty_person[2]))
-                        cleaned_person.append(str(dirty_person[2]))
-
-                    if self.__test_data(str(dirty_person[3]), patterns[3]):
-                        print("Valid Sales: " + str(dirty_person[3]))
-                        cleaned_person.append(str(dirty_person[3]))
-
-                    if self.__test_data(str(dirty_person[4]), patterns[4]):
-                        print("Valid BMI: " + str(dirty_person[4]))
-                        cleaned_person.append(str(dirty_person[4]))
-
-                    if self.__test_data(str(dirty_person[5]), patterns[5]):
-                        print("Valid salary: " + str(dirty_person[5]))
-                        cleaned_person.append(str(dirty_person[5]))
-
-                    if self.__test_data(str(dirty_person[6]), patterns[6]):
-                        print("Valid birthday: " + str(dirty_person[6]))
-                        cleaned_person.append(str(dirty_person[6]))
+                    # if self.__test_data(str(dirty_person[0]), patterns[0]):
+                    #     print("Valid empid: " + str(dirty_person[0]))
+                    #     cleaned_person.append(str(dirty_person[0]))
+                    #
+                    # if self.__test_data(str(dirty_person[1]), patterns[1]):
+                    #     print("Valid gender: " + str(dirty_person[1]))
+                    #     cleaned_person.append(str(dirty_person[1]))
+                    #
+                    # if self.__test_data(str(dirty_person[2]), patterns[2]):
+                    #     print("Valid age: " + str(dirty_person[2]))
+                    #     cleaned_person.append(str(dirty_person[2]))
+                    #
+                    # if self.__test_data(str(dirty_person[3]), patterns[3]):
+                    #     print("Valid Sales: " + str(dirty_person[3]))
+                    #     cleaned_person.append(str(dirty_person[3]))
+                    #
+                    # if self.__test_data(str(dirty_person[4]), patterns[4]):
+                    #     print("Valid BMI: " + str(dirty_person[4]))
+                    #     cleaned_person.append(str(dirty_person[4]))
+                    #
+                    # if self.__test_data(str(dirty_person[5]), patterns[5]):
+                    #     print("Valid salary: " + str(dirty_person[5]))
+                    #     cleaned_person.append(str(dirty_person[5]))
+                    #
+                    # if self.__test_data(str(dirty_person[6]), patterns[6]):
+                    #     print("Valid birthday: " + str(dirty_person[6]))
+                    #     cleaned_person.append(str(dirty_person[6]))
                 else:
                     return "Not enough feilds: " + str(len(dirty_person))
 
